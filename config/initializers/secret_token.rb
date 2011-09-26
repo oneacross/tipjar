f12da3cf64d46798082ab48eeb9ee007e5943296
Matt Mendell <matthew.mendell@gmail.com>
2011-09-25 23:39:01 -0700

    initial commit of tipjar!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tipjar::Application.config.secret_token = 'aaf7b4f7392ae3dd131f5be3ce445d9bd47566720bf5232b4648d8525864bdd4addd4f25c6f4ce7ee2b8b106d872c47b136557b8305d55fa3f8dc7e2f15135be'
