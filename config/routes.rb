c46b7acad744387ba1358de3e242fb82045d3291
Matt Mendell <matthew.mendell@gmail.com>
2011-10-05 23:32:20 -0700

    Add Requests controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tipjar::Application.routes.draw do
+  get "requests/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +13,7 @@ Tipjar::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :requests
 
   # Sample resource route with options:
   #   resources :products do
