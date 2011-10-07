1fd6de30ae43ef50bd5b5aa0860244cbfa8b95e2
Matt Mendell <matthew.mendell@gmail.com>
2011-10-06 22:13:56 -0700

    Change the cucumber request step to use the view!
    It passes!

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,4 +3,22 @@ class RequestsController < ApplicationController
     @requests = Request.all
   end
 
+  def new
+    @request = Request.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @request }
+    end
+  end
+
+  def create
+    @request = Request.new(params[:request])
+    if @request.save
+      redirect_to requests_index_path
+    else
+      render :action => 'new'
+    end
+  end
+
 end
--- features/step_definitions/tipjar_steps.rb
+++ features/step_definitions/tipjar_steps.rb
@@ -1,3 +1,7 @@
 Given /^I request (.*) to play in (.*) for (\d+) dollars$/ do |band, place, money|
-    Request.create(:band_name => band, :place => place, :money => money)
+  visit new_request_path
+  fill_in 'request_band_name', :with => band
+  fill_in 'request_place', :with => place
+  fill_in 'request_money', :with => money
+  click_button 'Create Request' 
 end
