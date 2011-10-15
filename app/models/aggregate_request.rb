319dad07798ba6f52f29969d5c3e57e2d982972d
Matt Mendell <matthew.mendell@gmail.com>
2011-10-14 23:02:09 -0700

    Create request partial
    Give requests some css!

--- app/models/aggregate_request.rb
+++ app/models/aggregate_request.rb
@@ -1,2 +1,18 @@
 class AggregateRequest < ActiveRecord::Base
+
+  def who
+    if count == 1
+      "#{count} person"
+    else
+      "#{count} people"
+    end
+  end
+
+  def verb
+    if count == 1
+      "wants"
+    else
+      "want"
+    end
+  end
 end
